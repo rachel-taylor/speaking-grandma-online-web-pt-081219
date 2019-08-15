b0c2fb7842566915251b097143f2c90e30b346fb
Rachel T <rachmaybe@gmail.com>
2019-08-15 01:22:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     else
-  		return "I LOVE YOU TOO PUMPKIN!"
+  		return "NO, NOT SINCE 1938!"
     end
 end
 
