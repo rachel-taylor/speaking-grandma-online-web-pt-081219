5fcadbd256953b4a9767c3bb07e51a5bfbf936ef
Rachel T <rachmaybe@gmail.com>
2019-08-15 01:23:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,8 +7,8 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-    else
-  		return "NO, NOT SINCE 1938!"
+    # else
+  		# return "NO, NOT SINCE 1938!"
     end
 end
 
