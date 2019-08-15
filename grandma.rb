2e365e0253e7510cb8249bb615cfb0b85ce7f174
Rachel T <rachmaybe@gmail.com>
2019-08-15 01:28:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,18 +7,3 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
-# def speak_to_grandma(phrase)
-#     if phrase == "I LOVE YOU GRANDMA!"
-#       return "I LOVE YOU TOO PUMPKIN!"
-#     elsif phrase == phrase.downcase
-#   		return "HUH?! SPEAK UP, SONNY!"
-#     # elsif phrase == phrase.downcase
-#     #   return "HUH?! SPEAK UP, SONNY!"
-#     elsif phrase == phrase.upcase
-#       return "NO, NOT SINCE 1938!"
-#     else
-#   		return "NO, NOT SINCE 1938!"
-#     end
-# end
-
-# speak_to_grandma("HELLO THERE")
