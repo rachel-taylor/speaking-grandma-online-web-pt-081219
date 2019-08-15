a1c0e700b41104a1e29c40b468a05ca57ef7d8a5
Rachel T <rachmaybe@gmail.com>
2019-08-15 01:02:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,5 @@
+def speak_to_grandma
+end 
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -11,17 +13,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
 def speak_to_grandma
 name = "Alice"
  
-if name == "Alice"
-  puts "Hello, Alice!"
-elsif name == "The White Rabbit"
-  puts "Don't be late, White Rabbit"
-elsif name == "The Mad Hatter"
-  puts "Welcome to the tea party, Mad Hatter"
-elsif name == "The Queen of Hearts"
-  puts "Please don't chop off my head!"
-else
-  puts "Whoooo are you?"
-end 
